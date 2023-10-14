7c9a68a8b7b9c1e4463cd61d5682257468ce271f
hossenmaruf <severusmaruf740@gmail.com>
2023-10-15 03:50:47 +0600

    labels ..

--- GUI window.py
+++ GUI window.py
@@ -8,4 +8,21 @@ icon = PhotoImage(file='logo.png')
 window.iconphoto(True,icon)
 window.config(background="#5cfcff")
 
+
+photo = PhotoImage(file='download.png')
+
+label = Label(window,
+              text="PYTHON?",
+              font=('Arial',40,'bold'),
+              fg='#00FF00',
+              bg='black',
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              image=photo,
+              compound='bottom')
+label.pack()
+#label.place(x=0,y=0)
+
 window.mainloop() #place window on computer screen, listen for events
