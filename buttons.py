a01803c01c37f895a1e9712c640b8ce658bfb3c3
hossenmaruf <severusmaruf740@gmail.com>
2023-10-21 02:37:58 +0600

    buttons use for increasing number

--- buttons.py
+++ buttons.py
@@ -1,14 +1,29 @@
 from tkinter import *
 
+# button = you click it, then it does stuff
+
+count = 0
+
 def click():
-    print("you press the button")
+    global count
+    count+=1
+    print(count)
 
 window = Tk()
 
-button  = Button(window,
-                 text  = "click me!",
-                 command= click)
+photo = PhotoImage(file='download.png')
 
+button = Button(window,
+                text="click me!",
+                command=click,
+                font=("Comic Sans",30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE,
+                image=photo,
+                compound='bottom')
 button.pack()
-window.mainloop()
 
+window.mainloop()
