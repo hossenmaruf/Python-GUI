daa1cf6af1f2c7bdbd6ce86316acd40d159c80db
hossenmaruf <85754212+hossenmaruf@users.noreply.github.com>
2024-02-10 02:50:27 +0600

    Add files via upload

---
+++ messageBox.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+
+def click():
+    messagebox.showinfo()
+
+window = Tk()
+
+button = Button(window, command =click, text="click me")
+button.pack()
+window.mainloop()
