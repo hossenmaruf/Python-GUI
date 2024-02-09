1f32f209ea4510587e5ff45ecaa7317296b4a28c
hossenmaruf <severusmaruf740@gmail.com>
2024-02-10 03:17:20 +0600

    Me

---
+++ messageBox.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+
+def click():
+    messagebox.showinfo(title='this is a info title box', message='you are a person')
+
+window = Tk()
+
+
+button = Button(window, command = click, text="click me")
+button.pack()
+window.mainloop()
